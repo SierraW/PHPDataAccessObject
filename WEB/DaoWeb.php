e043a8949a4fbf46a2f7966e24f4ff28e0db9e3a
Yiyao Zhang <me@yiyao.ca>
2020-07-17 16:34:53 -0400

    update, insert, delete

--- WEB/DaoWeb.php
+++ WEB/DaoWeb.php
@@ -9,9 +9,11 @@
 
 class DaoWeb{
 //messages
+    private $data = 'data';
     private $message = 'success';
-    private $success = 'success';
+    private $succeeded = 'success';
     private $failed = 'failed';
+    private $debug_msg = 'message';
     protected $conn;
 
     function  __construct($conn){
@@ -19,23 +21,27 @@ class DaoWeb{
     }
 
     function isEmpty($result) {
-        if ($result[$this->message] === $this->failed || empty($result['data'])) {
+        if ($result[$this->message] === $this->failed || empty($result[$this->data])) {
             return true;
         }
         return false;
     }
 
+    function isSuccess($result) {
+        return $result[$this->message] === $this->succeeded;
+    }
+
     function success() {
-        $data[$this->message] = $this->success;
-        $data['data'] = array();
-        $data['message'] = 'success';
+        $data[$this->message] = $this->succeeded;
+        $data[$this->data] = array();
+        $data[$this->debug_msg] = $this->succeeded;
         return $data;
     }
 
-    function failed($msg) {
+    function fail($msg) {
         $data[$this->message] = $this->failed;
-        $data['data'] = array();
-        $data['message'] = 'failed at: ' . $msg;
+        $data[$this->data] = array();
+        $data[$this->debug_msg] = 'failed at: ' . $msg;
         return $data;
     }
 
@@ -43,18 +49,18 @@ class DaoWeb{
         $result = $this->conn->query($sql);
 
         if (!isset($result->num_rows)) {
-            return $this->failed($sql);
+            return $this->fail($sql);
         }
 
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-                $data['data'][] = $row;
+                $data[$this->data][] = $row;
             }
-            $data[$this->message] = $this->success;
+            $data[$this->message] = $this->succeeded;
         } else {
-            $data['data'] = array();
-            $data['message'] = 'no such result';
-            $data[$this->message] = $this->success;
+            $data[$this->data] = array();
+            $data[$this->debug_msg] = 'no such result';
+            $data[$this->message] = $this->succeeded;
         }
 
         return $data;
@@ -69,18 +75,18 @@ class DaoWeb{
         $result = $stmt->get_result();
 
         if (!isset($result->num_rows)) {
-            return $this->failed('no data');
+            return $this->fail('no data');
         }
 
         if ($result->num_rows > 0) {
             while($row = $result->fetch_assoc()) {
-                $data['data'][] = $row;
+                $data[$this->data][] = $row;
             }
-            $data[$this->message] = $this->success;
+            $data[$this->message] = $this->succeeded;
         } else {
-            $data['data'] = array();
-            $data['message'] = 'no such result';
-            $data[$this->message] = $this->success;
+            $data[$this->data] = array();
+            $data[$this->debug_msg] = 'no such result';
+            $data[$this->message] = $this->succeeded;
         }
 
         return $data;
@@ -91,10 +97,13 @@ class DaoWeb{
         if ($this->conn->query($sql) === TRUE) {
             return $this->success();
         } else {
-            return $this->failed($sql);
+            return $this->fail($sql);
         }
     }
 
+    /**
+     * @deprecated replaced with insert($table_name, $data_map)
+     */
     function insertSQLFromMap($tableName, $data_map) {
         $upperSql = "INSERT INTO `".$tableName."`(";
         $lowerSql = ") VALUES (";
@@ -117,12 +126,79 @@ class DaoWeb{
         if ($stmt->execute() === TRUE) {
             $result = $this->success();
         } else {
-            $result = $this->failed('safe insert');
+            $result = $this->fail('safe insert');
         }
 
         $stmt->close();
         $this->conn->close();
 
         return $result;
+    }
+
+    function select($table_name, $select_row_name, $condition_sql) {
+        $sql = "SELECT ";
+        if ($select_row_name == null) {
+            $sql .= "* ";
+        } else {
+            $isFirst = true;
+            foreach ($select_row_name as $item) {
+                if ($isFirst) {
+                    $isFirst = false;
+                } else {
+                    $sql .= ", ";
+                }
+                $sql .= $item;
+            }
+        }
+        $sql .= " FROM {$table_name}";
+        if ($condition_sql == null) {
+            return $this->getData($sql);
+        } else {
+            $sql .= " WHERE {$condition_sql}";
+            return $this->getData($sql);
+        }
+    }
+
+    function delete($table_name, $condition_sql) {
+        if ($condition_sql == null) {
+            $sql = 'DELETE FROM `'.$table_name.'` WHERE 1';
+            return $this->insertData($sql);
+        }
+        $sql = 'DELETE FROM `'.$table_name.'` WHERE ' . $condition_sql;
+        return $this->insertData($sql);
+    }
+
+    function update($table_name, $data_map, $condition_sql) {
+        $upperSql = "UPDATE `{$table_name}` SET ";
+        $lowerSql = ' WHERE ' . $condition_sql;
+        $isFirst = true;
+        foreach ($data_map as $name=>$value) {
+            if ($isFirst) {
+                $isFirst = false;
+            } else {
+                $upperSql.= ', ';
+            }
+            $upperSql .= "`{$name}` = '{$value}'";
+        }
+        $sql = $upperSql . $lowerSql;
+        return $this->insertData($sql);
+    }
+
+    function insert($table_name, $data_map) {
+        $upperSql = "INSERT INTO `".$table_name."`(";
+        $lowerSql = ") VALUES (";
+        $count = 0;
+        foreach ($data_map as $name => $value) {
+            if ($count == 0) {
+                $count++;
+            } else {
+                $upperSql .= ", ";
+                $lowerSql .= ", ";
+            }
+            $upperSql .= "`{$name}`";
+            $lowerSql .= "'{$value}'";
+        }
+        $sql = $upperSql . $lowerSql . ")";
+        return $this->insertData($sql);
     }
 }
